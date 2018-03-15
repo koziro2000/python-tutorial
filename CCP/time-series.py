1c267b63b455992d4920e4d33c71d673c582f362
koziro2000 <koziro2000@gmail.com>
2018-03-15 11:29:13 -0400

    commit

---
+++ CCP/time-series.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 10:18:39 2018
+
+@author: jroh
+"""
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('Quarter_End_ITD_Batch_20180301_134403.csv')
+

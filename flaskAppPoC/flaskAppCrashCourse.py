a91c58002969b2e4f456493faea4221dcbb57a4f
koziro2000 <koziro2000@gmail.com>
2018-04-21 19:14:39 -0400

    file upload example
    file upload example

--- flaskAppPoC/flaskAppCrashCourse.py
+++ flaskAppPoC/flaskAppCrashCourse.py
@@ -4,21 +4,48 @@ Created on Sat Apr 21 05:37:31 2018
 
 @author: jroh
 """
-
+import os
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
+app.config['TESTING'] = True
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
+
+anyOfPasswords = ['password', 'secret']
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=\
                            [InputRequired(message='A user name is required'),\
                             Length(min=5, max=10, message='Must be between 5 and 10')])
-    password = PasswordField('password',  validators=[InputRequired('A password is required')])
+    password = PasswordField('password',  validators=[InputRequired('A password is required')\
+                                                      , AnyOf(values=anyOfPasswords)])
+    
 
+    
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileAllowed(photos, u'Image only!')\
+                                  , FileRequired(u'File was empty!')])   
+    submit = SubmitField(u'Upload')
+    
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
+    else:
+        file_url = None
+    return render_template('fileUpload.html', form=form, file_url=file_url)
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = LoginForm()

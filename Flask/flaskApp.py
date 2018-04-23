0facc3050695ec4d3bad5ffd06543639492997b9
koziro2000 <koziro2000@gmail.com>
2018-04-23 08:51:26 -0400

    latest change from vd
    vd including tax and some Flask Examples

---
+++ Flask/flaskApp.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 12:41:35 2018
+
+@author: jroh
+"""
+
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DtaRequired
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello':'world'}
+    
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Tax/taxToXML.py
+++ Tax/taxToXML.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import contextlib, os
+import numpy as np
 
 strInputXlsFile = "T1204.xlsx"
 strInputWS = "2017 T1204 Values"
@@ -16,6 +17,31 @@ x1 = x1.drop(x1.index[[1, 2, 3, 4, 5]])
 #Rename columns with first record
 x1.columns = x1.iloc[0]
 x1 = x1.iloc[1:]
+
+xmlBeginning = '<?xml version="1.0" encoding="UTF-8"?> '+\
+'\n<Submission xmlns:ccms="http://www.cra-arc.gc.ca/xmlns/ccms/1-0-0" ' +\
+ ' xmlns:sdt="http://www.cra-arc.gc.ca/xmlns/sdt/2-2-0" ' +\
+ ' xmlns:ols="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols/1-0-1" ' +\
+ ' xmlns:ols1="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols1/1-0-1"' +\
+ ' xmlns:ols10="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols10/1-0-1"' +\
+ ' xmlns:ols100="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols100/1-0-1"' +\
+ ' xmlns:ols12="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols12/1-0-1"' +\
+ ' xmlns:ols125="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols125/1-0-1"' +\
+ ' xmlns:ols140="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols140/1-0-1"' +\
+ ' xmlns:ols141="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols141/1-0-1"' +\
+ ' xmlns:ols2="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols2/1-0-1"' +\
+ ' xmlns:ols5="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols5/1-0-1"' +\
+ ' xmlns:ols50="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols50/1-0-1"' +\
+ ' xmlns:ols52="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols52/1-0-1"' +\
+ ' xmlns:ols6="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols6/1-0-1"' +\
+ ' xmlns:ols8="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols8/1-0-1"' +\
+ ' xmlns:ols8-1="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols8-1/1-0-1"' +\
+ ' xmlns:ols9="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/ols9/1-0-1"' +\
+ ' xmlns:olsbr="http://www.cra-arc.gc.ca/enov/ol/interfaces/efile/partnership/olsbr/1-0-1"' +\
+ ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +\
+ ' xsi:noNamespaceSchemaLocation="layout-topologie.xsd">'
+
+
 
 #T619 node
 def T619(row):
@@ -45,52 +71,64 @@ def T619(row):
     xml.append('<cntc_nm>{0}</cntc_nm>'.format('Cindy S. Lee'))
     xml.append('<cntc_area_cd>{0}</cntc_area_cd>'.format('416'))
     xml.append('<cntc_phn_nbr>{0}</cntc_phn_nbr>'.format('479-5531'))
-    xml.append('<cntc_extn_nbr>{0}</cntc_extn_nbr>'.format(''))
+    #xml.append('<cntc_extn_nbr>{0}</cntc_extn_nbr>'.format(''))
     xml.append('<cntc_email_area>{0}</cntc_email_area>'.format('cindy.lee@cppib.com'))
-    xml.append('<sec_cntc_email_area>{0}</sec_cntc_email_area>'.format('cindy.lee@cppib.com'))
+    #xml.append('<sec_cntc_email_area>{0}</sec_cntc_email_area>'.format('cindy.lee@cppib.com'))
     xml.append('</CNTC>')
     
     xml.append('</T619>')
     return '\n'.join(xml)
 
 
+def appendXML(xml, tag, value):
+    tagString = '<'+tag+'><![CDATA[{0}]]></'+tag+'>';
+    if (pd.notnull(value)):
+        xml.append(tagString.format(value))
+    return xml
+
+#T1204 Slip Generator
 def T1204Slip(row):
     xml = ['<T1204Slip>']
-    xml.append('<RCPNT_NUM>')
-    xml.append('<snm>{0}</snm>'.format(row['Sole proprietorship recipient last name ']))
-    xml.append('<gvn_nm>{0}</gvn_nm>'.format(row['Sole proprietorship recipient first name ']))
-    xml.append('<init>{0}</init>'.format(row['Sole proprietorship recipient initial ']))
-    xml.append('</RCPNT_NUM>')
-    xml.append('<sin>{0}</sin>'.format(row['Recipient social insurance number (SIN) ']))
-    xml.append('<rcpnt_bn>{0}</rcpnt_bn>'.format(row['Recipient Account Number ']))
+    
+    if(pd.notnull(row['Sole proprietorship recipient last name ']) or 
+       pd.notnull(row['Sole proprietorship recipient first name ']) or
+       pd.notnull(row['Sole proprietorship recipient first name '])):
+        xml.append('<RCPNT_NUM>')
+        appendXML(xml, 'snm', row['Sole proprietorship recipient last name '])    
+        appendXML(xml, 'gvn_nm', row['Sole proprietorship recipient first name '])    
+        appendXML(xml, 'init', row['Sole proprietorship recipient initial '])
+        xml.append('</RCPNT_NUM>')
+
+    appendXML(xml, 'sin', row['Recipient social insurance number (SIN) '])    
+    appendXML(xml, 'rcpnt_bn', row['Recipient Account Number '])
     
     xml.append('<RCPNT_BUS_NM>')
-    xml.append('<l1_nm>{0}</l1_nm>'.format(row['Recipient business name - line 1 ']))
-    xml.append('<l2_nm>{0}</l2_nm>'.format(row['Recipient business name - line 2 ']))
+    appendXML(xml, 'l1_nm', row['Recipient business name - line 1 '])
+    appendXML(xml, 'l2_nm', row['Recipient business name - line 2 '])
     xml.append('</RCPNT_BUS_NM>')
     
-    xml.append('<rcpnt_tcd>{0}</rcpnt_tcd>'.format(row['Recipient type code ']))
+    appendXML(xml, 'rcpnt_tcd', row['Recipient type code '])
    
     xml.append('<RCPNT_BUS_ADDR>')
-    xml.append('<addr_l1_txt>{0}</addr_l1_txt>'.format(row['Recipient business address - line 1 ']))
-    xml.append('<addr_l2_txt>{0}</addr_l2_txt>'.format(row['Recipient business address - line 2 ']))
-    xml.append('<cty_nm>{0}</cty_nm>'.format(row['Recipient city ']))
-    xml.append('<prov_cd>{0}</prov_cd>'.format(row['Recipient province or territory code ']))
-    xml.append('<cntry_cd>{0}</cntry_cd>'.format(row['Recipient country code ']))
-    xml.append('<pstl_cd>{0}</pstl_cd>'.format(row['Recipient postal code ']))
+    appendXML(xml, 'addr_l1_txt', row['Recipient business address - line 1 '])
+    appendXML(xml, 'addr_l2_txt', row['Recipient business address - line 2 '])
+    appendXML(xml, 'cty_nm', row['Recipient city '])
+    appendXML(xml, 'prov_cd', row['Recipient province or territory code '])
+    appendXML(xml, 'cntry_cd', row['Recipient country code '])
+    appendXML(xml, 'pstl_cd', row['Recipient postal code '])
     xml.append('</RCPNT_BUS_ADDR>')
 
-    xml.append('<payr_bn>{0}</payr_bn>'.format(row['Payer Business Number (BN) ']))
+    appendXML(xml, 'payr_bn',row['Payer Business Number (BN) '])
 
     xml.append('<T1204_AMT>')
 
-    xml.append('<srvc_pay_amt>{0}</srvc_pay_amt>'.format(row['Service payments only ']))
-    xml.append('<mxd_gd_pay_amt>{0}</mxd_gd_pay_amt>'.format(row['Mixed services and goods payments ']))
+    appendXML(xml, 'srvc_pay_amt',row['Service payments only '])
+    appendXML(xml, 'mxd_gd_pay_amt',row['Mixed services and goods payments '])
 
     xml.append('</T1204_AMT>')
 
-    xml.append('<ptnrp_filr_id>{0}</ptnrp_filr_id>'.format(row['Partnership filer identification number (FIN) ']))
-    xml.append('<rpt_tcd>{0}</rpt_tcd>'.format(row[18]))
+    appendXML(xml, 'ptnrp_filr_id',row['Partnership filer identification number (FIN) '])
+    appendXML(xml, 'rpt_tcd',row[18])
 
 
     xml.append('</T1204Slip>')
@@ -98,40 +136,39 @@ def T1204Slip(row):
     return '\n'.join(xml)
 
 def T1204Summary(row):
-    xml = ['<T1204Slip>']
-    xml.append('<T1204Summary>')
-    xml.append('<bn>{0}</bn>'.format(row['Business Number (BN) ']))
+    xml = ['\n<T1204Summary>']
+    appendXML(xml, 'bn',row['Business Number (BN) '])
 
     xml.append('<PAYR_NM>')
-    xml.append('<l1_nm>{0}</l1_nm>'.format(row['Payer name - line 1 ']))
-    xml.append('< l2_nm>{0}</l2_nm>'.format(row['Payer name - line 2 ']))
-    xml.append('<l3_nm>{0}</l3_nm>'.format(row['Payer name - line 3 ']))
+    appendXML(xml, 'l1_nm',row['Payer name - line 1 '])
+    appendXML(xml, 'l2_nm',row['Payer name - line 2 '])
+    appendXML(xml, 'l3_nm',row['Payer name - line 3 '])
     xml.append('</PAYR_NM>')
 
     xml.append('<PAYR_ADDR>')
-    xml.append('<addr_l1_txt>{0}</addr_l1_txt>'.format(row['Payer address - line 1 ']))
-    xml.append('<addr_l2_txt>{0}</addr_l2_txt>'.format(row['Payer address - line 2 ']))
-    xml.append('<cty_nm>{0}</cty_nm>'.format(row['Payer city ']))
-    xml.append('< prov_cd>{0}</prov_cd>'.format(row['Payer province or territory code ']))
-    xml.append('<cntry_cd>{0}</cntry_cd>'.format(row['Payer country code ']))
-    xml.append('<pstl_cd>{0}</pstl_cd>'.format(row['Payer postal code ']))
+    appendXML(xml, 'addr_l1_txt', row['Payer address - line 1 '])
+    appendXML(xml, 'addr_l2_txt', row['Payer address - line 2 '])
+    appendXML(xml, 'cty_nm', row['Payer city '])
+    appendXML(xml, 'prov_cd', row['Payer province or territory code '])
+    appendXML(xml, 'cntry_cd', row['Payer country code '])
+    appendXML(xml, 'pstl_cd', row['Payer postal code '])
     xml.append('</PAYR_ADDR>')
 
     xml.append('<CNTC>')
-    xml.append('<cntc_nm>{0}</cntc_nm>'.format(row['Contact name ']))
-    xml.append('< cntc_area_cd>{0}</cntc_area_cd>'.format(row['Contact area code ']))
-    xml.append('<cntc_phn_nbr>{0}</cntc_phn_nbr>'.format(row['Contact telephone number']))
-    xml.append('< cntc_extn_nbr>{0}</cntc_extn_nbr>'.format(row['Contact extension ']))
+    appendXML(xml, 'cntc_nm', row['Contact name '])
+    appendXML(xml, 'cntc_area_cd', row['Contact area code '])
+    appendXML(xml, 'cntc_phn_nbr', row['Contact telephone number'])
+    appendXML(xml, 'cntc_extn_nbr', row['Contact extension '])
     xml.append('</CNTC>')
 
-    xml.append('< tx_yr>{0}</tx_yr>'.format(row['Taxation year ']))
-    xml.append('<slp_cnt>{0}</slp_cnt>'.format(row['Total number of T1204 slip records ']))
-    xml.append('< rpt_tcd>{0}</rpt_tcd>'.format(row['Report Type Code ']))
-    xml.append('<fileramendmentnote></fileramendmentnote>'.format(row['Filer amendment note\xa0 ']))
+    appendXML(xml, 'tx_yr', row['Taxation year '])
+    appendXML(xml, 'slp_cnt', row['Total number of T1204 slip records '])
+    appendXML(xml, 'rpt_tcd', row['Report Type Code '])
+    appendXML(xml, 'fileramendmentnote', row['Filer amendment note\xa0 '])
 
     xml.append('<T1204_TAMT>')
-    xml.append('<tot_srvc_pay_amt>{0}</tot_srvc_pay_amt>'.format(row['Total service payments ']))
-    xml.append('<tot_mxd_gd_pay_amt>{0}</tot_mxd_gd_pay_amt>'.format(row['Total mixed services and goods payments ']))
+    appendXML(xml, 'tot_srvc_pay_amt', row['Total service payments '])
+    appendXML(xml, 'tot_mxd_gd_pay_amt', row['Total mixed services and goods payments '])
 
     xml.append('</T1204_TAMT>')
 
@@ -145,7 +182,9 @@ x1Summary = x1.iloc[0:1, 19:]
 xmlSummary = '\n'.join(x1Summary.apply(T1204Summary, axis=1))
 xml619 = T619(1)
 xml1 = '\n'.join(x1.apply(T1204Slip, axis=1))
-xml1 = '<Submission>\n' + xml619 + '\n<Return>\n<T1204>\n' + xml1 + xmlSummary + '\n<\T1204>\n</Return>\n</Submission>' 
+#xml1 = xmlBeginning + '\n<Submission>\n' + xml619 + '\n<Return>\n<T1204>\n' + xml1 + xmlSummary + '\n</T1204>\n</Return>\n</Submission>' 
+
+xml1 = xmlBeginning + '\n' + xml619 + '\n<Return>\n<T1204>\n' + xml1 + xmlSummary + '\n</T1204>\n</Return>\n</Submission>' 
 
 filename = 'sampleT1024.xml';
 with contextlib.suppress(FileNotFoundError):
@@ -153,35 +192,3 @@ with contextlib.suppress(FileNotFoundError):
 
 with open(filename, 'a') as the_file:
     the_file.write(xml1)
-
-###
-
-
-# Summary
-
-'Business Number (BN) ', 
-
-'Payer name - line 1 ', 
-'Payer name - line 2 ',
-'Payer name - line 3 ', 
-
-'Payer address - line 1 ',
-'Payer address - line 2 ', 
-'Payer city ',
-'Payer province or territory code ', 
-'Payer country code ',
-'Payer postal code ', 
-
-'Contact name ', 
-'Contact area code ',
-'Contact telephone number', 
-'Contact extension ', 
-
-'Taxation year ',
-'Total number of T1204 slip records ', 
-'Report Type Code ',
-
-'Filer amendment note  ', 
-'Total service payments ',
-'Total mixed services and goods payments '
-###

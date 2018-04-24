8308e93a19695e6fb4dba6ca3777a5a264454301
koziro2000 <koziro2000@gmail.com>
2018-04-23 22:47:58 -0400

    sample mongodb application for RealEstate PoC, document included

---
+++ flaskAppPoC/realEstateRpt.py
@@ -0,0 +1,51 @@
+"""
+    this PoC is to constrcut those screens for RE
+"""
+
+"""
+    General Partner Information
+
+
+    {
+        "Property":"Westfield Topanga",
+        "Portfolio":"Westfield JV",
+        "Property Type": "Super Regional Mall",
+        "Acquisition Date": "March 2012",
+        "Stabilization Date": "March 2012",
+        "Year Built/Last Renovated": "1965/2008",
+        "Location":"Los Angeles, CA",
+        "NRA/GLA/Units":"1,554,776",
+        "Lifecycle":"Operating - Near term renovation required",
+        "Ownership Type":"Minority JV Interest",
+        "CPPIB Ownership (%)":"45%",
+        "Ground Lease (Expiration, Options)":"N/A"
+    }
+
+
+{"_id":"5ade917d88e80b2420fe3685","property":"westfield topanga","portfolio":"westfield JV","Property Type":"Super Regional Mall","Acquisition Date":"March 2012","Stabilization Date":"March 2012","Year Built/Last Renovated":"1965/2008","Location":"Los Angeles, CA","NRA/GLA/Units":"1,554,776","Lifecycle":"Operating - Near term renovation required","Ownership Type":"Minority JV Interest","CPPIB Ownership (%)":"45%","Ground Lease (Expiration, Options)":"N/A"}
+
+"""
+
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+
+client = MongoClient('localhost', 27017)
+db = client.conquer
+generalPartners = db.GeneralPartner
+
+app = Flask(__name__)
+title = "General Partner Information"
+heading = "Asset"
+
+@app.route("/")
+def showGeneralPartner():
+    generalPartnerSet = generalPartners.find({"_id":ObjectId('5ade917d88e80b2420fe3685')})
+    print(generalPartnerSet)
+    return render_template('showGeneralPartner.html', generalPartners = generalPartnerSet, t=title, h=heading)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# Careful with the debug mode..
